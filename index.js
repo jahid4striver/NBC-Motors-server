8f81186959ce79705f1e7fc7300b0b5ff3af83c5
jahid4striver <jahidhassn701@gmail.comr>
2022-05-01 10:22:37 +0600

    bikes added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const app= express();
-const port= process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // Middleware
 
@@ -9,10 +11,41 @@ app.use(cors())
 app.use(express.json())
 
 
-app.get('/', (req, res)=>{
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.khoup.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    await client.connect();
+    const bikeCollection = client.db('nbcMotors').collection('bikes');
+
+    try {
+
+        app.get('/bikes', async (req, res) => {
+            const query = {};
+            const cursor = bikeCollection.find(query);
+            const result = await cursor.toArray();
+            res.send({success: 'Find Succesfully', data: result});
+        })
+
+
+    } catch (error) {
+
+    }
+}
+
+run();
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+app.get('/', (req, res) => {
     res.send('NBC Server is Running')
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('Listening to Port', port);
 })
